ee5ed1cd2a3a4c9ed99b260b067b420d88da2f7b
Denil Parada <paradadeniljose@gmail.com>
2025-02-13 21:19:20 -0600

    creacion del programa

---
+++ programasLisp.lisp
@@ -0,0 +1,45 @@
+;;; =====================================================================
+;;; Universidad del Valle de Guatemala
+;;; Autor: Denil José Parada Cabrera - 24761, Joel Nerio, Jose Rivera
+;;; Fecha: 13 de Febrero de 2025
+;;; Descripción: Programa en Lisp que contiene tres funciones:
+;;;             - Conversión de grados Celsius a Fahrenheit.
+;;;             - Cálculo de la serie de Fibonacci hasta el número n.
+;;;             - Cálculo del factorial de un número n.
+;;; =====================================================================
+
+;;; Función para convertir grados Celsius a Fahrenheit
+(defun celsius-a-fahrenheit (celsius)
+  "Convierte una temperatura de grados Celsius a Fahrenheit."
+  (+ (* celsius 1.8) 32))
+
+;;; Prueba de la función Celsius a Fahrenheit
+(format t "~%30 grados Celsius en Fahrenheit: ~a~%" (celsius-a-fahrenheit 30))
+
+
+
+
+;;; =====================================================================
+;;; Función para calcular la serie de Fibonacci hasta el número n
+(defun fibonacci (n)
+  "Calcula el enésimo número de la serie de Fibonacci."
+  (if (<= n 1)
+      n
+      (+ (fibonacci (- n 1)) (fibonacci (- n 2)))))
+
+;;; Prueba de la función Fibonacci
+(format t "~%Fibonacci de 10: ~a~%" (fibonacci 10))
+
+
+
+
+;;; =====================================================================
+;;; Función para calcular el factorial de un número n
+(defun factorial (n)
+  "Calcula el factorial de un número dado n."
+  (if (<= n 1)
+      1
+      (* n (factorial (- n 1)))))
+
+;;; Prueba de la función Factorial
+(format t "~%Factorial de 5: ~a~%" (factorial 5))
